64ee7a04c287aa76054a1467009e2334b73a9210
leopii98 <rippergames98@gmail.com>
2025-11-09 16:14:22 +0100

    add-3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,7 +1,23 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
-// Dichiara la funzione qui.
+const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
+
+// Dichiara la funzione qui.
+function filtraPerLettera(array, lettera) {
+    const risultato = [];
+
+    for (let i = 0; i < array.length; i++) {
+        const nome = array[i];
+        if (nome[0].toLowerCase() === lettera.toLowerCase()) {
+            risultato.push(nome);
+        }
+    }
+
+    return risultato;
+}
 // Invoca la funzione qui e stampa il risultato in console
+const filtrati = filtraPerLettera(names, 'A');
+console.log(filtrati);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
